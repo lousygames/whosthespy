9b787a0282115aed65033f5c5e2ea3a6c3d4905f
Mario Castigliano <mario.castigliano@outlook.it>
2023-10-17 00:12:44 +0200

    fix def language

--- script/index.mjs
+++ script/index.mjs
@@ -4,11 +4,11 @@ window.addEventListener("load", () => {
   const langSelector = document.querySelector("#lang_selector");
   langSelector.addEventListener("input", (event) => {
     localizeUI(event.target.value);
-    localStorage.setItem('lang', event.target.value);
+    localStorage.setItem("lang", event.target.value);
   });
 
   const defLang = localStorage.getItem("lang");
 
   localizeUI(defLang || "en");
-  document.querySelector('#' + defLang || "en").checked = true;
+  document.querySelector("#" + (defLang || "en")).checked = true;
 });

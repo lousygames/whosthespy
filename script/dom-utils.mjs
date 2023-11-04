5933a7cddec9d2a2e459f98a839b8d599c597aaa
Mario Castigliano <mario.castigliano@outlook.it>
2023-11-04 16:36:03 +0100

    major code refactor

---
+++ script/dom-utils.mjs
@@ -0,0 +1,111 @@
+export function initializeDom(ui, payload, cards) {
+  const gameOn = document.createElement("h1");
+  gameOn.innerText = ui.play;
+  document.querySelector(".scene").appendChild(gameOn);
+
+  document.querySelector(".counter_label").innerText = ui.player;
+  document.querySelector(".counter").classList.remove("complete");
+  document.querySelector(
+    ".counter_ratio"
+  ).innerText = `1/${payload.gameSettings.playersCount}`;
+  document.querySelector(".counter").classList.add("hydrated");
+
+  cards = cards.map((e) => createCardElement(e, ui));
+  cards.forEach((element) => {
+    document.querySelector(".scene").appendChild(element);
+  });
+}
+
+function createCardElement(content, localizedUi) {
+  //Prepare card front
+  const cardFaceFront = document.createElement("div");
+  cardFaceFront.classList.add("card__face", "card__face--front");
+
+  const cardFrontParag = document.createElement("p");
+  cardFrontParag.innerText = localizedUi.title;
+  cardFrontParag.classList.add("title");
+
+  const cardFrontParagSmall = document.createElement("p");
+  cardFrontParagSmall.innerText = localizedUi.tap;
+  cardFrontParagSmall.classList.add("small_text");
+
+  cardFaceFront.appendChild(cardFrontParag);
+  cardFaceFront.appendChild(cardFrontParagSmall);
+
+  //Prepare card back
+  const cardFaceBack = document.createElement("div");
+  cardFaceBack.classList.add("card__face", "card__face--back");
+
+  const cardBackParag = document.createElement("p");
+  cardBackParag.innerText = content;
+  cardBackParag.classList.add("title");
+
+  const cardBackParagSmall = document.createElement("p");
+  cardBackParagSmall.innerText = localizedUi.nextPlayer;
+  cardBackParagSmall.classList.add("small_text");
+
+  cardFaceBack.appendChild(cardBackParag);
+  cardFaceBack.appendChild(cardBackParagSmall);
+
+  //Prepare card
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.appendChild(cardFaceFront);
+  card.appendChild(cardFaceBack);
+
+  //Click listener
+  card.addEventListener("click", (event) => cardClicked(event));
+
+  //Disable click while animation runs
+  card.addEventListener(
+    "transitionstart",
+    () => (card.dataset.isRunningTransition = true)
+  );
+
+  //Enable click after animation is completed and remove previous card from dom
+  card.addEventListener("transitionend", (event) => {
+    const c = event.target;
+    delete c.dataset.isRunningTransition;
+    if (c.dataset.isRemoved) {
+      c.parentElement.removeChild(c);
+    }
+  });
+
+  return card;
+}
+
+function cardClicked(event) {
+  //Find outer card div
+  let card = event.target;
+  while (!card.classList.contains("card")) {
+    card = card.parentElement;
+  }
+
+  //If animation is running don't do anything
+  if (card.dataset.isRunningTransition) {
+    return;
+  }
+
+  //If card is yet to be flipped, flip it
+  if (!card.dataset.isFlipped) {
+    card.dataset.isFlipped = true;
+    return;
+  }
+
+  //Otherwise update players count, and remove current card
+  const count = JSON.parse(localStorage.getItem("payload")).gameSettings
+    .playersCount;
+  const remainingCards =
+    Array.from(document.querySelectorAll(".card")).length - 1;
+
+  if (count - remainingCards + 1 <= count) {
+    document.querySelector(".counter_ratio").innerText = `${
+      count - remainingCards + 1
+    }/${count}`;
+  } else {
+    //If there's no more cards to draw, remove the counter
+    document.querySelector(".counter").classList.add("complete");
+  }
+  card.dataset.isRemoved = true;
+}
--- script/play.mjs
+++ script/play.mjs
@@ -1,8 +1,17 @@
-window.addEventListener("load", async () => {
-  let ui;
-  await fetch("assets/localization/ui.json")
-    .then((response) => response.json())
-    .then((_ui) => (ui = _ui));
+import { shuffle, extractRandomElementFrom } from "./array-utils.mjs";
+import { initializeDom } from "./dom-utils.mjs";
+
+const topics = {
+  0: "geo",
+  1: "loc",
+  2: "nat",
+  3: "anm",
+  4: "job",
+  5: "obj",
+};
+
+const main = async () => {
+  //If app is not initialized go back to home
   const payload = JSON.parse(localStorage.getItem("payload"));
   if (
     !payload ||
@@ -13,167 +22,45 @@ window.addEventListener("load", async () => {
   ) {
     window.location.href = "/index.html";
   }
-  const localizedUi = ui[payload.lang];
 
+  const ui = (
+    await fetch("assets/localization/ui.json").then((response) =>
+      response.json()
+    )
+  )[payload.lang];
+
+  const words = await fetch(
+    `assets/localization/assets.${payload.lang}.json`
+  ).then((response) => response.json());
   let pool = [];
 
-  fetch(`assets/localization/assets.${payload.lang}.json`)
-    .then((response) => response.json())
-    .then((json) => {
-      payload.gameSettings.topics.forEach((element) => {
-        const topicName = topics[element];
-        pool = pool.concat(...json[topicName]);
-      });
-      const value = extractFrom(pool);
-      let cards = generateCards(
-        payload.gameSettings.playersCount,
-        payload.gameSettings.spiesCount,
-        payload.lang,
-        value
-      );
+  payload.gameSettings.topics.forEach((element) => {
+    const topicName = topics[element];
+    pool = pool.concat(...words[topicName]);
+  });
+  const value = extractRandomElementFrom(pool);
+  let cards = generateCards(
+    payload.gameSettings.playersCount,
+    payload.gameSettings.spiesCount,
+    value,
+    ui.spy
+  );
 
-      const werePlaying = document.createElement("h1");
-      werePlaying.innerText = localizedUi.play;
-      document.querySelector(".scene").appendChild(werePlaying);
-
-      document.querySelector(".counter_label").innerText = localizedUi.player;
-      document.querySelector(".counter").classList.remove("complete");
-      document.querySelector(
-        ".counter_ratio"
-      ).innerText = `1/${payload.gameSettings.playersCount}`;
-      document.querySelector(".counter").classList.add("hydrated");
-
-      cards = cards.map((e) => createCardElement(e));
-      cards.forEach((element) => {
-        document.querySelector(".scene").appendChild(element);
-      });
-    });
-
-  function createCardElement(content) {
-    const card = document.createElement("div");
-    const cardFaceFront = document.createElement("div");
-    const cardFaceBack = document.createElement("div");
-    const frontParag = document.createElement("p");
-    const frontSmallParag = document.createElement("p");
-    const backParag = document.createElement("p");
-    const backSmallParag = document.createElement("p");
-
-    card.classList.add("card");
-    cardFaceFront.classList.add("card__face", "card__face--front");
-    cardFaceBack.classList.add("card__face", "card__face--back");
-    frontParag.innerText = localizedUi.title;
-    frontParag.classList.add("title");
-    frontSmallParag.innerText = localizedUi.tap;
-    frontSmallParag.classList.add("small_text");
-    backParag.innerText = content;
-    backParag.classList.add("title");
-    backSmallParag.innerText = localizedUi.nextPlayer;
-    backSmallParag.classList.add("small_text");
-
-    cardFaceFront.appendChild(frontParag);
-    cardFaceFront.appendChild(frontSmallParag);
-    cardFaceBack.appendChild(backParag);
-    cardFaceBack.appendChild(backSmallParag);
-    card.appendChild(cardFaceFront);
-    card.appendChild(cardFaceBack);
-
-    card.addEventListener("click", (event) => cardClicked(event));
-    card.addEventListener(
-      "transitionstart",
-      () => (card.dataset.isRunningTransition = true)
-    );
-    card.addEventListener("transitionend", (event) => {
-      const c = event.target;
-      delete c.dataset.isRunningTransition;
-      if (c.dataset.isRemoved) {
-        c.parentElement.removeChild(c);
-      }
-    });
-
-    return card;
-  }
-
-  function cardClicked(event) {
-    let card = event.target;
-
-    while (!card.classList.contains("card")) {
-      card = card.parentElement;
-    }
-    if (card.dataset.isRunningTransition) {
-      return;
-    }
-
-    if (!card.dataset.isFlipped) {
-      card.dataset.isFlipped = true;
-      return;
-    }
-
-    const count = JSON.parse(localStorage.getItem("payload")).gameSettings
-      .playersCount;
-    const remainingCards =
-      Array.from(document.querySelectorAll(".card")).length - 1;
-    if (count - remainingCards + 1 <= count) {
-      document.querySelector(".counter_ratio").innerText = `${
-        count - remainingCards + 1
-      }/${count}`;
-    } else {
-      document.querySelector(".counter").classList.add("complete");
-    }
-    card.dataset.isRemoved = true;
-  }
-
-  function generateCards(player, spies, lang, value) {
-    const cards = [];
-
-    for (let i = 0; i < player - spies; i++) {
-      cards.push(value);
-    }
-
-    for (let j = 0; j < spies; j++) {
-      cards.push(localizedUi.spy);
-    }
-    shuffle(cards);
-    return cards;
-  }
-});
-function extractFrom(array) {
-  const coolDownWords = JSON.parse(localStorage.getItem("cooldown")) || [];
-  if (coolDownWords?.length) {
-    coolDownWords.forEach((element) => {
-      const i = array.indexOf(element);
-      if (i >= 0) {
-        array.splice(i, 1);
-      }
-    });
-  }
-  array = shuffle(array);
-  const extr = array[getRandomInt(array.length)];
-  coolDownWords.push(extr);
-  if (coolDownWords.length > 20) {
-    coolDownWords.shift();
-  }
-
-  localStorage.setItem("cooldown", JSON.stringify(coolDownWords));
-  return extr;
-}
-
-export const topics = {
-  0: "geo",
-  1: "loc",
-  2: "nat",
-  3: "anm",
-  4: "job",
-  5: "obj",
+  initializeDom(ui, payload, cards);
 };
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function generateCards(players, spies, value, spy) {
+  const cards = [];
+
+  for (let i = 0; i < players - spies; i++) {
+    cards.push(value);
+  }
+
+  for (let j = 0; j < spies; j++) {
+    cards.push(spy);
+  }
+  shuffle(cards);
+  return cards;
 }
 
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
+window.onload = main;
